7729a3617684266988b6d2a0214ac2d1e4537e55
Tiantian Li <tiantiancscs@gmail.com>
2024-09-25 14:40:05 +1000

    Update Queue.py

--- Queue.py
+++ Queue.py
@@ -1,3 +1,14 @@
 from collections import deque
 
- 
+class Queue:
+    def __init__(self):
+        self.queue = deque()
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.queue.popleft()
+        return None  # Return None if the queue is empty
+
