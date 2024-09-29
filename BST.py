99117f9e3ffc5a89a99124bdfcfccd6b23248b6e
Tiantian Li <tiantiancscs@gmail.com>
2024-09-30 09:51:56 +1000

    Update BST.py

--- BST.py
+++ BST.py
@@ -7,3 +7,22 @@ class TreeNode:
 class BinarySearchTree:
     def __init__(self):
         self.root = None
+
+    def insert(self, value):
+        if self.root is None:
+            self.root = TreeNode(value)
+        else:
+            self._insert_recursive(self.root, value)
+
+    def _insert_recursive(self, node, value):
+        if value < node.value:
+            if node.left is None:
+                node.left = TreeNode(value)
+            else:
+                self._insert_recursive(node.left, value)
+        else:
+            if node.right is None:
+                node.right = TreeNode(value)
+            else:
+                self._insert_recursive(node.right, value)
+
